e535199653f40a1c2496f4ddf48eb828b1adf424
ThomasLEBRET <th.lebret@outlook.fr>
2021-02-28 17:40:26 +0100

    datas : rewrite get, post methods and add add errors controller

---
+++ code/class/datas/Request.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("Parameters.php");
+
+class Request {
+
+    private $get;
+
+    private $post;
+
+
+    public function __construct() {
+        $this->get = new Parameters($_GET);
+        $this->post = new Parameters($_POST);
+    }
+
+    /**
+     * Retourne le contenu de la variable super globale $_GET encapsulé dans l'objet Parameters
+     * @return array le tableau associatif $_GET dans l'objet Parameters
+     */
+    public function getGet() {
+        return $this->get;
+   }
+
+   /**
+    * Retourne le contenu de la variable super globale $_POST encapsulé dans l'objet Parameters
+    * @return array le tableau associatif $_POST dans l'objet Parameters
+    */
+    public function getPost() {
+       return $this->post;
+   }
+}
