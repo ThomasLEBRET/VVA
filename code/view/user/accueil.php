cf10e4732748fb1e661f56d272cf5e84668f6c62
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-02 03:13:26 +0100

    create views for log user and display result

--- code/view/user/accueil.php
+++ code/view/user/accueil.php
@@ -1,25 +1,21 @@
-<?php $title = "Erreur 404" ?>
+<?php $title = "VVA - Espace connexion" ?>
 
 <?php ob_start(); ?>
 
 <div class="jumbotron jumbotron-fluid justify">
   <div class="container">
     <h1 class="display-4">Bienvenu sur le site de VVA.</h1>
-    <form>
+    <form action="index.php?page=accueil" method="POST">
       <div class="form-group">
         <label for="exampleInputEmail1">Identifiant</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="idHelp" placeholder="Entrer votre identifiant">
+        <input name="login" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="idHelp" placeholder="Entrer votre identifiant">
         <small id="idHelp" class="form-text text-muted">Ce dernier est unique et sert à vous identifier</small>
       </div>
       <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <label for="exampleInputPassword1">Mot de passe</label>
+        <input name="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
       </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
+      <button type="submit" class="btn btn-primary">Envoyer</button>
     </form>
   </div>
 </div>
