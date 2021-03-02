bf957026ede62dba1ba406a9fa635768d4910c12
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-02 03:14:25 +0100

    model class refactoring and add connection function to log user (call by controller)

--- code/class/user/User.php
+++ code/class/user/User.php
@@ -1,147 +1,34 @@
 <?php
 
-class User {
+require_once("class/datas/Database.php");
 
-  private $user;
-  private $mdp;
-  private $nomcompte;
-  private $prenomcompte;
-  private $dateinscrip;
-  private $dateferme;
-  private $typeprofil;
-  private $datedebsejour;
-  private $datefinsejour;
-  private $datenaiscompte;
-  private $adrmailcompte;
-  private $notelcompte;
+class User extends Database {
 
-  public function __construct() {
-    $user = "unknow";
-    $mdp = "unknow";
-    $nomcompte = "unknow";
-    $prenomcompte = "unknow";
-    $dateinscrip = "unknow";
-    $dateferme = "unknow";
-    $typeprofil = "unknow";
-    $datedebsejour = "unknow";
-    $datefinsejour = "unknow";
-    $datenaiscompte = "unknow";
-    $adrmailcompte = "unknow";
-    $notelcompte = "unknow";
+  public function __get($key) {
+    $method = 'get'.ucfirst($key);
+    $this->$key = $this->$method();
+    return $this->$key;
   }
 
-  /**
-  * Construit un objet en fonction d'un tableau de paramètres
-  * @param  array  $params un tableau associatif de paramètres composant l'objet User
-  * @return User         un objet de type User
-  */
-  private function buildObject(array $params) {
-    foreach ($params as $key => $value) {
-      $method = 'set'.ucfirst(strtolower($key));
-      if(method_exists($this, $method)) {
-        $this->$method($value);
-        $_SESSION[$key] = $value;
-      }
+  public function connexion($params) {
+    $login = $params->get("login");
+    $password = $params->get("password");
+    $sql =
+    "
+    SELECT *
+    FROM COMPTE
+    WHERE USER = ?
+      AND MDP = ?
+    ";
+    $user = $this->prepare($sql, [$login, $password], 'User', 1);
+    if(empty($user)) {
+      return false;
+    } else {
+      Session::set('USER', $user->USER);
+      Session::set('TYPEPROFIL', $user->TYPEPROFIL);
+      return true;
     }
-    return $this;
   }
-
-  public function getUser(){
-    return $this->user;
-  }
-
-  public function setUser($user){
-    $this->user = $user;
-  }
-
-  public function getMdp(){
-    return $this->mdp;
-  }
-
-  public function setMdp($mdp){
-    $this->mdp = $mdp;
-  }
-
-  public function getNomcompte(){
-    return $this->nomcompte;
-  }
-
-  public function setNomcompte($nomcompte){
-    $this->nomcompte = $nomcompte;
-  }
-
-  public function getPrenomcompte(){
-    return $this->prenomcompte;
-  }
-
-  public function setPrenomcompte($prenomcompte){
-    $this->prenomcompte = $prenomcompte;
-  }
-
-  public function getDateinscrip(){
-    return $this->dateinscrip;
-  }
-
-  public function setDateinscrip($dateinscrip){
-    $this->dateinscrip = $dateinscrip;
-  }
-
-  public function getDateferme(){
-    return $this->dateferme;
-  }
-
-  public function setDateferme($dateferme){
-    $this->dateferme = $dateferme;
-  }
-
-  public function getTypeprofil(){
-    return $this->typeprofil;
-  }
-
-  public function setTypeprofil($typeprofil){
-    $this->typeprofil = $typeprofil;
-  }
-
-  public function getDatedebsejour(){
-    return $this->datedebsejour;
-  }
-
-  public function setDatedebsejour($datedebsejour){
-    $this->datedebsejour = $datedebsejour;
-  }
-
-  public function getDatefinsejour(){
-    return $this->datefinsejour;
-  }
-
-  public function setDatefinsejour($datefinsejour){
-    $this->datefinsejour = $datefinsejour;
-  }
-
-  public function getDatenaiscompte(){
-    return $this->datenaiscompte;
-  }
-
-  public function setDatenaiscompte($datenaiscompte){
-    $this->datenaiscompte = $datenaiscompte;
-  }
-
-  public function getAdrmailcompte(){
-    return $this->adrmailcompte;
-  }
-
-  public function setAdrmailcompte($adrmailcompte){
-    $this->adrmailcompte = $adrmailcompte;
-  }
-
-  public function getNotelcompte(){
-    return $this->notelcompte;
-  }
-
-  public function setNotelcompte($notelcompte){
-    $this->notelcompte = $notelcompte;
-  }
-
 
 }
 
