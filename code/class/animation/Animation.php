5fd7159fbeae3f670f85fc8c7c4afff5379c3fa8
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-03 18:51:45 +0100

    create 2 types of get all animations. "Encadrant" can get all animations and "vacancier" can get just valides animation

--- code/class/animation/Animation.php
+++ code/class/animation/Animation.php
@@ -5,12 +5,25 @@ include("sqlRequests.php");
 
 class Animation extends Database {
 
-  public function getAnimationsValides() {
+  public function getAll() {
 
-    global $getAnimationsValides;
-    $animations = $this->prepare($getAnimationsValides, [], 'Animation', 0);
+    global $getAll;
+    global $getAllValides;
+
+    if(Session::get('TYPEPROFIL') == 'EN') {
+      $animations = $this->prepare($getAll, [], 'Animation', 0);
+    } else {
+      $dtFinSej = Session::get('DATEFINSEJOUR');
+      $dtDebSej = Session::get('DATEDEBSEJOUR');
+
+      $dateNaiss = new DateTime(Session::get('DATENAISCOMPTE'));
+      $dateNow = new DateTime(date("Y-m-d"));
+
+      $ageUser = $dateNow->diff($dateNaiss);
+      $animations = $this->prepare($getAllValides, [$dtFinSej, $ageUser->y], 'Animation', 0);
+    }
     return $animations;
-    
+
   }
 
 }
