f42bfcbc8fdcb0ea04c3af4172007fa5b85c8a9f
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-03 19:52:42 +0100

    add more informations

--- code/class/animation/Animation.php
+++ code/class/animation/Animation.php
@@ -20,7 +20,7 @@ class Animation extends Database {
       $dateNow = new DateTime(date("Y-m-d"));
 
       $ageUser = $dateNow->diff($dateNaiss);
-      $animations = $this->prepare($getAllValides, [$dtFinSej, $ageUser->y], 'Animation', 0);
+      $animations = $this->prepare($getAllValides, [$ageUser->y], 'Animation', 0);
     }
     return $animations;
 
--- code/class/animation/sqlRequests.php
+++ code/class/animation/sqlRequests.php
@@ -1,16 +1,17 @@
 <?php
   $getAll =
     "
-      SELECT *
-      FROM ANIMATION
+      SELECT AN.*, Nb_place_rest(CODEANIM) as places_restantes, TA.NOMTYPEANIM
+      FROM ANIMATION AN, TYPE_ANIM TA
+      WHERE AN.CODETYPEANIM = TA.CODETYPEANIM
     ";
 
   $getAllValides =
     "
-      SELECT AN.*
-      FROM ANIMATION AN, ACTIVITE A
+      SELECT AN.*, TA.NOMTYPEANIM, Nb_place_rest(AN.CODEANIM) as places_restantes
+      FROM ANIMATION AN, ACTIVITE A, TYPE_ANIM TA
       WHERE AN.CODEANIM = A.CODEANIM
-      AND ? >= DATE(NOW())
+      AND AN.CODETYPEANIM = TA.CODETYPEANIM
       AND AN.LIMITEAGE <= ?
       AND Nb_place_rest(AN.CODEANIM) > 0
       AND A.DATEANNULEACT IS NULL
