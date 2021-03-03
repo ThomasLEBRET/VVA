8576c5b2d7db21d2535624f46cc941e3a04680cb
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-03 18:50:50 +0100

    sql request to get all animations and get valides animations

--- code/class/animation/sqlRequests.php
+++ code/class/animation/sqlRequests.php
@@ -1,7 +1,19 @@
 <?php
-  $getAnimationsValides =
+  $getAll =
     "
       SELECT *
       FROM ANIMATION
+    ";
+
+  $getAllValides =
+    "
+      SELECT AN.*
+      FROM ANIMATION AN, ACTIVITE A
+      WHERE AN.CODEANIM = A.CODEANIM
+      AND ? >= DATE(NOW())
+      AND AN.LIMITEAGE <= ?
+      AND Nb_place_rest(AN.CODEANIM) > 0
+      AND A.DATEANNULEACT IS NULL
+      GROUP BY AN.CODEANIM
     ";
 ?>
