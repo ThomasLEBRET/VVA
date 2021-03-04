169f0b025c5179accf4c5f46c8b4830731a9183e
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-04 23:56:07 +0100

    add controller to add animation and condition to se all animations for encadrant

--- code/class/animation/AnimationController.php
+++ code/class/animation/AnimationController.php
@@ -12,8 +12,35 @@ class AnimationController extends Animation {
 
   public function allAnimations() {
     $animations = $this->animation->getAll();
+    if(Session::get('TYPEPROFIL') == 'EN') {
+      $codesTypeAnimation = $this->animation->getCodesTypeAnimations();
+    }
     require_once("view/animation/allAnimations.php");
   }
+
+  public function addAnimation($post) {
+    if(Session::get('TYPEPROFIL') == 'EN') {
+      $allIsset = true;
+      foreach ($post->getArray() as $key => $value) {
+        if(empty($value)) {
+          $allIsset = false;
+        }
+      }
+      if($allIsset) {
+        if($this->animation->add($post)) {
+          $addSuccess = "L'animation a bien été ajoutée";
+          require_once("view/animation/allAnimations.php");
+        } else {
+          require_once("view/animation/error/errorAdd.php");
+        }
+      } else {
+        require_once("view/animation/error/errorAdd.php");
+      }
+    } else {
+      require_once("view/animation/error/errorUser.php");
+    }
+  }
+
 }
 
 ?>
