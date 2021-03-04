2dcbdf150029d4432a42fab385685d22cabab31f
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-03-04 10:08:38 +0100

    add content for code error activite view

--- code/view/activite/errors/errorCodeAnimation.php
+++ code/view/activite/errors/errorCodeAnimation.php
@@ -2,6 +2,12 @@
 
 <?php ob_start(); ?>
 
+<div class="jumbotron jumbotron-fluid justify">
+    <div class="container">
+        <h1 class="display-4">Erreur : Code d'animation non trouvé ou invalide</h1>
+        <p class="lead">Le code saisie n'est pas valide, veuillez nous en excuser.</p>
+    </div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require("view/template.php"); ?>
