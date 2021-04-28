634e7449ce23a0f805627563d66cea2ea11da98c
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-04-28 17:56:27 +0200

    add function to debug more efficiently

--- code/view/errors/500.php
+++ code/view/errors/500.php
@@ -1,11 +1,11 @@
 <?php $title = "Erreur 500" ?>
 
 <?php ob_start(); ?>
-<?php echo $e ?>
 <div class="jumbotron jumbotron-fluid justify">
     <div class="container">
         <h1 class="display-4">Erreur 500</h1>
         <p class="lead">Une erreur serveur est survenue</p>
+        <?php echo "<p><pre>".$e."</pre></p>"; ?>
     </div>
 </div>
 
